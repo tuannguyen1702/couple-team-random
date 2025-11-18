3b9c4b3705470d4f0495ac9b30ed59f7e337c0c6
tuannguyen1702 <tuannguyen1702@gmail.com>
2025-11-18 16:05:51 +0700

    Update page.tsx

--- src/app/vote/page.tsx
+++ src/app/vote/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useRef } from 'react'
 import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { cn } from '@/lib/utils'
 
 const MEMBERS = [
